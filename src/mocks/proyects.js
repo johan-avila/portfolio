c8864346b81ceca5a130273843190ab8327f1f35
johan-avila <johan-avila@outlook.com>
2020-11-11 04:57:35 -0600

    Add new proyects

--- src/mocks/proyects.js
+++ src/mocks/proyects.js
@@ -19,18 +19,14 @@ const proyectsMock = [
         ]
     },
     {
-        title: "News Inc App",
-        link: `https://news-inc.web.app/#/`,
-        body : "Una Periodico online que hace scraping a 5 peiodicos diferentes, construido con el stack MERN y adicionalmente usa GraphQL",
+        title: "Plugin Media Player",
+        link: `https://johan-avila.github.io/pluginMediaPlayer/`,
+        body : "Plugin para una aplicacion de streamig como Netflix o Youtube, fue construido con Vanilla js y Sass, tambien se mudo unaparte del codigo a TypeScript",
         img: {
-            link: `https://media0.giphy.com/media/ULEzhSVBdgXNlN3wHq/giphy.gif`,
-            alt: "Giff de News Inc App"
+            link: `https://media.giphy.com/media/XovYzHmSl0hnKuHQ5f/giphy.gif`,
+            alt: "Giff del plugin de video"
         },
         technologies: [
-            {
-                link: `https://montykamath.files.wordpress.com/2018/02/graphql.png?w=210&h=210`,
-                alt: "Graphql logo",
-            },
             {
                 link: `https://www.mindrops.com/images/nodejs-image.png`,
                 alt: "Node js logo",
@@ -38,23 +34,34 @@ const proyectsMock = [
         ]
     },
     {
-        title: "News Inc App",
-        link: `https://news-inc.web.app/#/`,
-        body : "Una Periodico online que hace scraping a 5 peiodicos diferentes, construido con el stack MERN y adicionalmente usa GraphQL",
+        title: "Desempleado",
+        link: `https://johan-avila.github.io/desempleo/#/`,
+        body : "Es una single-page aplication construida con crete-react app",
         img: {
-            link: `https://media0.giphy.com/media/ULEzhSVBdgXNlN3wHq/giphy.gif`,
-            alt: "Giff de News Inc App"
+            link: `https://media.giphy.com/media/K5CjdmACQvLKoJ92tQ/giphy.gif` ,
+            alt: "Giff de desempledos"
         },
         technologies: [
-            {
-                link: `https://montykamath.files.wordpress.com/2018/02/graphql.png?w=210&h=210`,
-                alt: "Graphql logo",
-            },
             {
                 link: `https://www.mindrops.com/images/nodejs-image.png`,
                 alt: "Node js logo",
             }
         ]
+    },
+    {
+                                                                    title: "Rick and Morty API",
+                                                                    link: `https://johan-avila.github.io/rickandmortyapi/#/`,
+                                                                    body : "Es una single-page aplication construida con React, consume el API publica de Rick and Morty",
+                                                                    img: {
+                                                                        link: `https://media.giphy.com/media/iJN9Lk7p36GbGm3COd/giphy.gif` ,
+                                                                        alt: "Giff Rick and Morty app"
+                                                                    },
+                                                                    technologies: [
+                                                                        {
+                                                                            link: `https://www.mindrops.com/images/nodejs-image.png`,
+                                                                            alt: "Node js logo",
+                                                                        }
+                                                                    ]
     },
 ]
 
